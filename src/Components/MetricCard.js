74395bef0cc0efa5795290b44e27d5a25ba8c191
prajjwal1609 <149517475+prajjwal1609@users.noreply.github.com>
2025-09-30 17:11:06 +0530

    MetricCard file created

---
+++ src/Components/MetricCard.js
@@ -0,0 +1,28 @@
+// src/components/MetricCard.js
+ 
+import React from 'react';
+import { Card, CardContent, Typography, Box } from '@mui/material';
+ 
+function MetricCard({ title, value, icon, color }) {
+  return (
+<Card sx={{ minWidth: 275, boxShadow: 3, borderRadius: 2 }}>
+<CardContent>
+        {/* Icon and Title */}
+<Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+<Box sx={{ color: color, mr: 1 }}>
+            {icon}
+</Box>
+<Typography color="text.secondary" gutterBottom>
+            {title}
+</Typography>
+</Box>
+        {/* Main Value */}
+<Typography variant="h4" component="div">
+          {value}
+</Typography>
+</CardContent>
+</Card>
+  );
+}
+ 
+export default MetricCard;
